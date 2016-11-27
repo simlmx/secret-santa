b8301e8c6c0c29bb12c7ba317d5e7eda86586e00
Julian Kuhlmann <jlnkuhlmann@gmail.com>
2016-11-27 11:11:30 +0100

    Removed outdated test.

--- tests/test_secret_santa.py
+++ tests/test_secret_santa.py
@@ -1,28 +1,14 @@
 from unittest import TestCase
 import os
-from secret_santa import parse_yaml, choose_receiver, Person, create_pairs
+from secret_santa import choose_receiver, Person, create_pairs
 
 class TestSecretSanta(TestCase):
-
-    def test_parse_yaml(self):
-        CONFIG_PATH = os.path.join(os.path.dirname(__file__), '../config.yml.template')
-        parsed = parse_yaml(yaml_path=CONFIG_PATH)
-        self.assertTrue(parsed['MESSAGE'])
-        self.assertTrue(parsed['PASSWORD'])
-        self.assertTrue(parsed['TIMEZONE'])
-        self.assertTrue(parsed['SMTP_PORT'])
-        self.assertTrue(parsed['DONT_PAIR'])
-        self.assertTrue(parsed['PARTICIPANTS'])
-        self.assertTrue(parsed['SUBJECT'])
-        self.assertTrue(parsed['FROM'])
-        self.assertTrue(parsed['SMTP_SERVER'])
-        self.assertTrue(parsed['USERNAME'])
 
     def test_chooses_legitimate_receiver(self):
         hans  = Person('Hans', 'hans@email.com', 'Uschi')
         uschi = Person('Uschi', 'uschi@email.com', None)
         frank = Person('Frank', 'frank@email.com', None)
-        
+
         receiver = choose_receiver(hans, [uschi, frank])
 
         self.assertEqual(receiver, frank)
